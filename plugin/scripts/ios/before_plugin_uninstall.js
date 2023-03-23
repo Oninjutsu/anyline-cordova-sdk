e1c5378f76f17ae3ad857d57e08ccf1699f7e200
Oninjutsu <65342582+Oninjutsu@users.noreply.github.com>
2023-03-23 11:35:53 +0800

    Delete plugin directory

--- plugin/scripts/ios/before_plugin_uninstall.js
+++
@@ -1,6 +0,0 @@
-var helper = require("./helper");
-
-module.exports = function(context) {
-    var xcodeProjectPath = helper.getXcodeProjectPath();
-    helper.removeShellScriptBuildPhase(context, xcodeProjectPath);
-};
